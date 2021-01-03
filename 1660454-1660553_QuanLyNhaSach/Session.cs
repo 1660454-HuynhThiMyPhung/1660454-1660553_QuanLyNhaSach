2c361cbc00f8c65f8a897f1adb57903a4eea88d6
Ngân Thị Thanh <76875556+ngantthithanh@users.noreply.github.com>
2021-01-03 23:47:36 +0700

    Thêm ramdom chữ

--- 1660454-1660553_QuanLyNhaSach/Session.cs
+++ 1660454-1660553_QuanLyNhaSach/Session.cs
@@ -20,5 +20,12 @@ namespace _1660454_1660553_QuanLyNhaSach
             }
             return str;
         }
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
     }
 }
